419dc8eb9f9e12a0ac90354efbf00474195c35e2
nfp <nfpuleo@gmail.com>
2025-06-12 20:17:11 -0500

    we tried

---
+++ script.js
@@ -0,0 +1,40 @@
+const questions = [
+    {questionOne: "What do you end every line of code with?",
+        answerArray: [":", ";", ",", "."],
+        answerIndex: 1,
+    },
+    {questionTwo: "What is two plus two?",
+        answerArray: ["1", "2", "3", "4"],
+        answerIndex: 3,
+    },
+    {questionThree:"What is the best pizza topping?",
+        answerArray: ["pepperoni", "sausage", "bacon", "cheese"],
+        answerIndex: 0,
+    },
+];
+
+const questionField = document.getElementById("question-field");
+const answer1Button = document.getElementById("answer1");
+const answer2Button = document.getElementById("answer2");
+const answer3Button = document.getElementById("answer3");
+
+const feedback = document.getElementById("feedback");
+
+questionField.innerHTML = questions.questionOne
+
+
+
+.addEventListener("click", function () {
+
+
+});
+
+
+
+ 
+
+
+
+
+
+//listeners
